045e60fe444add1c93e5e6db1a7785dff70b64ba
niaomingjian <niaomingjian@outlook.com>
2026-03-09 10:58:41 +0800

    add vcn

--- tf-vcn/outputs.tf
+++ tf-vcn/outputs.tf
@@ -12,7 +12,7 @@ output "nat-gateway-id" {
   description = "OCID for NAT gateway"
   value = module.vcn.nat_gateway_id
 }
-output "id-for-for-route-table-that-includes-the-nat-gateway" {
+output "id-for-route-table-that-includes-the-nat-gateway" {
   description = "OCID of the nat-route table - This route table has a nat gateway to be used for private subnets. This route table also has a service gateway."
   value = module.vcn.nat_route_id
 }
@@ -25,3 +25,32 @@ output "private-security-list-name" {
 output "private-security-list-OCID" {
   value = oci_core_security_list.private-security-list.id
 }
+
+# Outputs for public security list
+
+output "public-security-list-name" {
+  value = oci_core_security_list.public-security-list.display_name
+}
+output "public-security-list-OCID" {
+  value = oci_core_security_list.public-security-list.id
+}
+
+# Outputs for private subnet
+
+output "private-subnet-name" {
+  value = oci_core_subnet.vcn-private-subnet.display_name
+}
+output "private-subnet-OCID" {
+  value = oci_core_subnet.vcn-private-subnet.id
+}
+
+# Outputs for public subnet
+
+output "public-subnet-name" {
+  value = oci_core_subnet.vcn-public-subnet.display_name
+}
+output "public-subnet-OCID" {
+  value = oci_core_subnet.vcn-public-subnet.id
+}
+
+
